78b07d79cd39e5d92307b13e01d58b48d7e4f99a
Sabilah Mudrikah <sabilahmudrikah2018@gmail.com>
2025-05-26 14:17:07 +0800

    feat: add jframe(login, menu, kasir, transaksi)

--- src/model/Transaksi.java
+++ src/model/Transaksi.java
@@ -1,13 +1,14 @@
+package model;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package koneksi;
 
 /**
  *
- * @author munir
+ * @author HP
  */
-public class koneksi {
+public class Transaksi {
     
 }
